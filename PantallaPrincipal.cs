5be8979fae34c84de252df3fe0944bc6169ca29b
Derek Fernando Solis Amador <159720354+Derekfernand0@users.noreply.github.com>
2024-06-20 12:53:51 -0600

    prueva1

---
+++ PantallaPrincipal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica10fun2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStripComboBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void agregarDirecciopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
